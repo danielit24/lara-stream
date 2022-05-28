07e6d8538832681f2b85c58cd73f32656f42d20b
daniel <daniel.yohanes@mncgroup.com>
2022-05-29 01:29:22 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\MovieController;
+use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\TransactionController;
+
+
+use App\Http\Controllers\Member\DashboardController as MemberDashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('/payment-finish', 'member.payment-finish')->name('member.payment.finish');
+
+Route::group(['prefix' => 'member'], function () {
+    Route::get('/', [MemberDashboardController::class, 'index'])->name('member.dashboard');
+
+    Route::get('movie/{id}', [MemberMovieController::class, 'show'])->name('member.movie.detail');
+    Route::get('movie/{id}/watch', [MemberMovieController::class, 'watch'])->name('member.movie.watch');
+
+    Route::post('transaction', [MemberTransactionController::class, 'store'])->name('member.transaction.store');
+
+    Route::get('subscription', [UserPremiumController::class, 'index'])->name('member.user_premium.index');
+    Route::delete('subscription/{id}', [UserPremiumController::class, 'destroy'])->name('member.user_premium.destroy');
+
+    Route::get('logout', [MemberLoginController::class, 'logout'])->name('member.login.logout');
+});
+
+// Admin routes
+Route::get('/admin/login', [LoginController::class, 'index'])->name('admin.login');
+Route::post('/admin/login', [LoginController::class, 'authenticate'])->name('admin.login.auth');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['admin.auth']], function (){
+    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+    Route::get('/logout', [LoginController::class, 'logout'])->name('admin.login.logout');
+
+    Route::group(['prefix' => 'movie'], function () {
+        Route::get('/', [MovieController::class, 'index'])->name('admin.movie');
+
+        Route::get('/create', [MovieController::class, 'create'])->name('admin.movie.create');
+        Route::post('/store', [MovieController::class, 'store'])->name('admin.movie.store');
+
+        Route::get('/edit/{id}', [MovieController::class, 'edit'])->name('admin.movie.edit');
+        Route::put('/update/{id}', [MovieController::class, 'update'])->name('admin.movie.update');
+
+        Route::delete('destroy/{id}', [MovieController::class, 'destroy'])->name('admin.movie.destroy');
+    });
+
+    Route::get('/transaction', [TransactionController::class, 'index'])->name('admin.transaction');
+
+});
